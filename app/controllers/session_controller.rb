bcd0e23a6d4deb337cdc64e9270360a0bedbf39f
Billy F <willfira717@gmail.com>
2020-10-22 14:43:01 -0400

    Added authentication to Sessions Controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,2 +1,14 @@
 class SessionController < ApplicationController
+    def new
+    end 
+
+    def create
+        # finds the user then authenticates the user by comparing the password in the db vs the password the user entered
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            redirect_to @user 
+        else 
+            render login_path
+        end 
+    end 
 end
