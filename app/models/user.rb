dbf04bb3e0891d58d25ae67c57943fe60354b18c
Billy F <willfira717@gmail.com>
2020-10-23 14:54:35 -0400

    Added scope method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_many :reviews
     has_many :movies, through: :reviews
+
+    scope :most_reviews, -> { joins(:reviews).group(:count).first }
+
 end
